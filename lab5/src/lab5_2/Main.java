4ee98a4d003966054f42a59d1912b97c0bd13ad3
Otto Tovisi <tovisiotto@gmail.com>
2021-03-22 20:30:59 +0200

    continue with fillRandom

--- lab5/src/lab5_2/Main.java
+++ lab5/src/lab5_2/Main.java
@@ -1,4 +1,30 @@
 package lab5_2;
 
 public class Main {
+    public static void main(String[] args) {
+        double[][] d = { { 1, 2, 3 }, { 4, 5, 6 }, { 9, 1, 3} };
+        Matrix m0 = new Matrix(d);
+        System.out.println("m0 rows: " + m0.getRows()+", cols: " + m0.getColumns());
+        System.out.println("m0: ");
+        m0.printMatrix();
+        System.out.println();
+
+
+        Matrix m00 = new Matrix( m0 );
+        System.out.println("m00 is a copy of m0: ");
+        m00.printMatrix();
+        System.out.println("************************");
+
+
+        Matrix m1 = new Matrix(2, 3);
+        m1.fillRandom(1,2);
+        System.out.println("m1: ");
+        m1.printMatrix();
+        System.out.println();
+
+
+    }
+
+
+
 }
--- lab5/src/lab5_2/Matrix.java
+++ lab5/src/lab5_2/Matrix.java
@@ -4,4 +4,53 @@ public class Matrix {
     private double[][] data;
     private int rows;
     private int columns;
+
+    public Matrix(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.data = new double[rows][columns];
+
+    }
+
+    //ez mi lenne és hogy hívóik meg?
+    public Matrix(double[][] data) {
+        this.columns = data[0].length;
+        this.rows = data.length;
+        this.data = new double[rows][columns];
+
+        for (int i=0; i<this.rows; ++i) {
+            for (int j=0; j< this.columns; ++j) {
+                this.data[i][j] = data[i][j];
+            }
+        }
+    }
+
+    public Matrix(Matrix asd) {
+        this.rows = asd.getRows();
+        this.columns = asd.getColumns();
+        this.data = new double[rows][columns];
+
+        for (int i=0; i<this.rows; ++i) {
+            for (int j=0; j< this.columns; ++j) {
+                this.data[i][j] = asd.data[i][j];
+            }
+        }
+    }
+
+    public int getRows() {
+        return this.rows;
+    }
+
+    public int getColumns() {
+        return this.columns;
+    }
+
+    public void printMatrix() {
+        for (int i=0; i<this.rows; ++i) {
+            for (int j=0; j< this.columns; ++j) {
+                System.out.printf("%.0f ", this.data[i][j]);  //.1f for one decimal
+            }
+            System.out.print("\n");
+        }
+    }
 }
